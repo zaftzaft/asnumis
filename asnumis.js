b27c0ecae173111fb423492e209ab5797638948f
Aoki <zaftzaft7@gmail.com>
2018-02-18 18:10:35 +0900

    add usage, exit-code

--- asnumis.js
+++ asnumis.js
@@ -3,4 +3,26 @@
 
 const asn = require("./asn.json");
 
-console.log(asn[process.argv[2]]);
+const asnumis = n => {
+  return asn[n];
+};
+
+module.exports = asnumis;
+
+if(typeof require != "undefined" && require.main === module) {
+  let num = process.argv[2];
+  if(!num) {
+    console.error("usage: asnumis <as number>");
+    return process.exit(2);
+  }
+
+  let org = asnumis(num);
+  if(org) {
+    console.log(org);
+    process.exit(0);
+  }
+  else {
+    process.exit(1);
+  }
+}
+
